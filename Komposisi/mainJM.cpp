ee8efcf1e27a04586508d55e78810072d5286402
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2024-05-21 19:25:53 +0700

    int main

--- Komposisi/mainJM.cpp
+++ Komposisi/mainJM.cpp
@@ -2,3 +2,11 @@
 #include <string>
 using namespace std;
 
+#include "jantung.h"
+#include "manusia.h"
+
+int main() {
+	manusia* varManusia = new manusia("rizky");
+	delete varManusia;
+	return 0;
+}
